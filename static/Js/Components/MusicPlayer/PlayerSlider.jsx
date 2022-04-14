6a64d37987d9e73da281af39b21bc87b0727cedb
Malaury DUTOUR <=>
2022-04-14 18:48:17 +0200

    Modified pogress bar

--- static/Js/Components/MusicPlayer/PlayerSlider.jsx
+++ static/Js/Components/MusicPlayer/PlayerSlider.jsx
@@ -3,49 +3,53 @@ import Draggable from 'react-draggable';
 import PropTypes from 'prop-types';
 import useResize from './Helper/useResize';
 
-
 const PlayerSlider = ({ Time, EndTime, OnSliderChange }) => {
-	const ContainerRef = useRef(null);
-	const { width } = useResize(ContainerRef);
-	const [IsDragging, SetIsDragging] = useState(false);
+    const ContainerRef = useRef(null);
+    const { width } = useResize(ContainerRef);
+    const [IsDragging, SetIsDragging] = useState(false);
 
-	return (
-		<div ref={ContainerRef} style={{ width: '100%', height: '2px' }} id="Progress-container" className="mb-2">
-			<div
-				className={`Progress-bar ${IsDragging ? 'noTransition' : ''}`}
-				style={{ width: `${(Time * width) / EndTime}px`, height: '100%' }}
-			/>
+    return (
+        <div
+            ref={ContainerRef}
+            style={{ width: '100%' }}
+            id="Progress-container"
+            className="pb-1"
+            onClick={(e) => OnSliderChange((e.clientX * EndTime) / width)}
+        >
+            <div
+                className={`Progress-bar ${IsDragging ? 'noTransition' : ''}`}
+                style={{ width: `${(Time * width) / EndTime}px`, height: '2px' }}
+            />
 
-			<Draggable
-				axis="x"
-				bounds="parent"
-				position={{
-					x: (Time * width) / EndTime,
-					y: 0,
-				}}
-				positionOffset={{
-					x: -6,
-					y: 0,
-				}}
-				onStart={() => SetIsDragging(true)}
-				onDrag={(e, data) => {
-					OnSliderChange((data.x * EndTime) / width);
-				}}
-				onStop={() => SetIsDragging(false)}
-			>
-				<div className={`Progress-drag  ${IsDragging ? 'noTransition' : ''}`}>
-					<div className="Progress-ball" />
-				</div>
-			</Draggable>
-
-		</div>
-	);
+            <Draggable
+                axis="x"
+                bounds="parent"
+                position={{
+                    x: (Time * width) / EndTime,
+                    y: 0,
+                }}
+                positionOffset={{
+                    x: -6,
+                    y: 0,
+                }}
+                onStart={() => SetIsDragging(true)}
+                onDrag={(e, data) => {
+                    OnSliderChange((data.x * EndTime) / width);
+                }}
+                onStop={() => SetIsDragging(false)}
+            >
+                <div className={`Progress-drag  ${IsDragging ? 'noTransition' : ''}`}>
+                    <div className="Progress-ball" />
+                </div>
+            </Draggable>
+        </div>
+    );
 };
 
 PlayerSlider.propTypes = {
-	Time: PropTypes.number.isRequired,
-	EndTime: PropTypes.number.isRequired,
-	OnSliderChange: PropTypes.func.isRequired,
+    Time: PropTypes.number.isRequired,
+    EndTime: PropTypes.number.isRequired,
+    OnSliderChange: PropTypes.func.isRequired,
 };
 
 export default PlayerSlider;
