ad7fdea8b6dd2ff9dba819ba33793a1eb060e8b5
MalauD <malaury.dutour@gmail.com>
2022-08-17 10:16:00 +0200

    Fixed warning

--- static/Js/Components/Items/AlbumItemCard.jsx
+++ static/Js/Components/Items/AlbumItemCard.jsx
@@ -3,46 +3,49 @@ import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import MoreButton from './Helper/MoreButton';
 
-const AlbumItemCard = ({
-	onClick, Image, Name, Artist, ImageDz, children, MoreOptions, IsComplete,
-}) => (
-	<Card style={{ width: '18rem', cursor: 'pointer' }} className="m-2 AlbumItemCard">
-		{MoreOptions && (
-			<div className="MoreIconButton">
-				<MoreButton>{children}</MoreButton>
-			</div>
-		)}
-		{ImageDz ? <Card.Img variant="top" src={ImageDz} onClick={onClick} />
-			: <Card.Img variant="top" src={Image ? `data:image/jpeg;base64,${Image.toString('base64')}` : '/Ressources/noMusic.jpg'} />}
+const AlbumItemCard = ({ onClick, Image, Name, Artist, ImageDz, children, MoreOptions }) => (
+    <Card style={{ width: '18rem', cursor: 'pointer' }} className="m-2 AlbumItemCard">
+        {MoreOptions && (
+            <div className="MoreIconButton">
+                <MoreButton>{children}</MoreButton>
+            </div>
+        )}
+        {ImageDz ? (
+            <Card.Img variant="top" src={ImageDz} onClick={onClick} />
+        ) : (
+            <Card.Img
+                variant="top"
+                src={
+                    Image
+                        ? `data:image/jpeg;base64,${Image.toString('base64')}`
+                        : '/Ressources/noMusic.jpg'
+                }
+            />
+        )}
 
-		<Card.Body onClick={onClick}>
-			<Card.Title style={{ textAlign: 'center', fontStyle: IsComplete ? 'normal' : 'italic' }}>{Name}</Card.Title>
-			<Card.Text>{Artist}</Card.Text>
-		</Card.Body>
-
-	</Card>
+        <Card.Body onClick={onClick}>
+            <Card.Title style={{ textAlign: 'center', fontStyle: 'normal' }}>{Name}</Card.Title>
+            <Card.Text>{Artist}</Card.Text>
+        </Card.Body>
+    </Card>
 );
 
 AlbumItemCard.propTypes = {
-	onClick: PropTypes.func.isRequired,
-	Image: PropTypes.string,
-	ImageDz: PropTypes.string,
-	Name: PropTypes.string.isRequired,
-	Artist: PropTypes.string,
-	children: PropTypes.oneOfType([
-		PropTypes.arrayOf(PropTypes.node),
-		PropTypes.node,
-	]),
-	MoreOptions: PropTypes.bool,
-	IsComplete: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired,
+    Image: PropTypes.string,
+    ImageDz: PropTypes.string,
+    Name: PropTypes.string.isRequired,
+    Artist: PropTypes.string,
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]),
+    MoreOptions: PropTypes.bool,
 };
 
 AlbumItemCard.defaultProps = {
-	Image: undefined,
-	ImageDz: '',
-	Artist: '', // TODO Pass artist
-	children: <></>,
-	MoreOptions: false,
+    Image: undefined,
+    ImageDz: '',
+    Artist: '', // TODO Pass artist
+    children: <></>,
+    MoreOptions: false,
 };
 
 export default AlbumItemCard;
