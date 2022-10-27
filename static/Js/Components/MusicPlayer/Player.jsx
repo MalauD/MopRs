8621764a27a8a98a825ba2d42961392780a10042
Malaury <malaurydutour@outlook.fr>
2022-10-27 21:40:01 +0200

    exclude already played music from musics player auto play

--- static/Js/Components/MusicPlayer/Player.jsx
+++ static/Js/Components/MusicPlayer/Player.jsx
@@ -204,7 +204,7 @@ class PlayerConnected extends React.Component {
         const { NextMusic, ChangePlayingId, CurrentMusicId, AddMultipleMusics, MusicIds } =
             this.props;
         if (!NextMusic) {
-            Axios.post('/Music/Related', { MusicIds }).then((res) => {
+            Axios.post('/Music/Related', { MusicIds, Exclude: MusicIds }).then((res) => {
                 AddMultipleMusics(res.data.RelatedMusics);
                 if (res.data.length !== 0) {
                     ChangePlayingId(CurrentMusicId + 1);
