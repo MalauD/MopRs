dfc1aa6a582ff87829dff8e7df3fb08c3133c824
Malaury <malaurydutour@outlook.fr>
2022-10-27 18:20:39 +0200

    Improved deezer related tracks

--- src/suggestion/related.rs
+++ src/suggestion/related.rs
@@ -11,10 +11,30 @@ pub async fn get_related_to(base_music_ids: &Vec<i32>, limit: i32) -> Vec<i32> {
         .unwrap()
         .unwrap();
 
+    let related_artists = db
+        .get_artists(
+            &base_musics_artists
+                .iter()
+                .map(|a| a.related_artists.clone().unwrap_or_default())
+                .flatten()
+                .collect(),
+        )
+        .await
+        .unwrap_or_default()
+        .unwrap();
+
     let mut related_musics = Vec::new();
     for artist in base_musics_artists {
         related_musics.extend(artist.top_tracks.unwrap_or_default());
     }
+    let mut related_musics_ext = Vec::new();
+    for artist in related_artists {
+        related_musics_ext.extend(artist.top_tracks.unwrap_or_default());
+    }
+    related_musics_ext.shuffle(&mut thread_rng());
+    related_musics_ext.truncate(related_musics.len());
+
+    related_musics.extend(related_musics_ext);
     related_musics.sort();
     related_musics.dedup();
     related_musics.shuffle(&mut thread_rng());
