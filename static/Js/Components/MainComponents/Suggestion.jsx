3628a24c74319448b9128b7e72d5cbe9d21b875b
Malaury <malaurydutour@outlook.fr>
2024-01-21 18:04:02 +0100

    Improved suggestions music group

--- static/Js/Components/MainComponents/Suggestion.jsx
+++ static/Js/Components/MainComponents/Suggestion.jsx
@@ -1,22 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import MusicGroup from './Groups/MusicGroup';
+import ButtonIcon from '../Helper/ButtonIcon';
+
+const GetSuggestion = (memory, novelty, likeHistRatio, limit) =>
+    Axios.get(
+        `/api/me/suggestions?memory=${memory}&likeHistRatio=${likeHistRatio}&novelty=${novelty}&limit=${limit}`
+    ).then((res) => res.data);
+
+const PAGE_SIZE = 8;
+const MEMORY_MULTIPLIER = 3;
 
 function Suggestion() {
     const [isFetching, setIsFetching] = React.useState(false);
     const [suggestions, setSuggestions] = React.useState([]);
+    const [page, setPage] = useState(0);
 
-    React.useEffect(() => {
+    const addSuggestion = () => {
+        if (suggestions.length > 100) return;
+        GetSuggestion((page + 1) * PAGE_SIZE * MEMORY_MULTIPLIER, 0.3, 0.1, PAGE_SIZE)
+            .then((ApiResult) => {
+                setSuggestions([...suggestions, ...ApiResult]);
+                setPage(page + 1);
+            })
+            .catch(() => {});
+    };
+
+    const getSuggestion = () => {
         setIsFetching(true);
-        Axios.get('/api/me/suggestions?memory=100&likeHistRatio=0.25&novelty=0.2&limit=15').then(
-            (res) => {
-                setSuggestions(res.data);
+        setPage(0);
+        GetSuggestion(PAGE_SIZE * MEMORY_MULTIPLIER, 0.3, 0.1, PAGE_SIZE)
+            .then((ApiResult) => {
+                setSuggestions(ApiResult);
                 setIsFetching(false);
-            }
-        );
+                setPage(1);
+            })
+            .catch(() => {
+                setIsFetching(false);
+            });
+    };
+
+    const Accessories = [
+        <ButtonIcon
+            dataEva="flip-outline"
+            onClick={getSuggestion}
+            evaOptions={{
+                fill: '#d6d6d6ff',
+                width: '30px',
+                height: '30px',
+            }}
+        />,
+    ];
+
+    useEffect(() => {
+        getSuggestion();
     }, []);
 
-    return <MusicGroup Musics={suggestions} title="Suggestions" isLoading={isFetching} />;
+    return (
+        <MusicGroup
+            Musics={suggestions}
+            title="Suggestions"
+            isLoading={isFetching}
+            showMore={suggestions.length < 100}
+            Accessories={Accessories}
+            onMoreClick={addSuggestion}
+        />
+    );
 }
 
 export default Suggestion;
